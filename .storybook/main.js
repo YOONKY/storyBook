e0a7b1635f0ceda9104737fcbdfa85905ca93db5
YOONKY <lov012726@gmail.com>
2022-09-01 16:20:22 +0900

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: [
+    "../src/Intro.stories.mdx",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+  ],
+  framework: "@storybook/react",
+  features: {
+    interactionsDebugger: true,
+  },
+};
